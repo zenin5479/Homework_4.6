9469f94fcf73936f2983ee3273ab183612483ed6
zenin5479 <zenin5479@gmail.com>
2025-06-15 22:35:51 +0300

    Update 4.7 Обновлен метод. Проведен рефакторинг

--- ClassFor1DArray.cs
+++ ClassFor1DArray.cs
@@ -26,6 +26,7 @@ namespace Homework_4._6
       public static double[] VvodArray(string path, string nameArray)
       {
          string stroka = null;
+         double[] arrayDouble = { };
          FileStream filestream = File.Open(path, FileMode.Open, FileAccess.Read);
          StreamReader streamReader = new StreamReader(filestream);
          while (streamReader.Peek() >= 0)
@@ -35,19 +36,24 @@ namespace Homework_4._6
          }
 
          streamReader.Close();
-         //Console.WriteLine();
-         Console.WriteLine("Исходный строковый массив {0}", nameArray);
-         Console.WriteLine(stroka);
-
-         // Определение количества столбцов в строке разделением строки на подстроки по пробелу
-         // Символ пробела
-         char symbolSpace = ' ';
-         // Счетчик символов
-         int symbolСount = 0;
-         // Количество столбцов в строке
-         int сolumn = 0;
-         if (stroka != null)
+         if (stroka == null)
          {
+            Console.WriteLine("Исходный строковый массив {0} пуст", nameArray);
+            return arrayDouble;
+         }
+         else
+         {
+            //Console.WriteLine();
+            Console.WriteLine("Исходный строковый массив {0}", nameArray);
+            Console.WriteLine(stroka);
+
+            // Определение количества столбцов в строке разделением строки на подстроки по пробелу
+            // Символ пробела
+            char symbolSpace = ' ';
+            // Счетчик символов
+            int symbolСount = 0;
+            // Количество столбцов в строке
+            int сolumn = 0;
             while (symbolСount < stroka.Length)
             {
                if (symbolSpace == stroka[symbolСount])
@@ -64,20 +70,17 @@ namespace Homework_4._6
             }
 
             //Console.WriteLine("Количество столбцов {0}", сolumn);
-         }
 
-         // Разделение строки на подстроки по пробелу и конвертация подстрок в double
-         Console.WriteLine("Массив вещественных чисел {0}", nameArray);
-         // Одномерный массив вещественных чисел
-         double[] arrayDouble = new double[сolumn];
-         // Построитель строк
-         StringBuilder stringModified = new StringBuilder();
-         // Счетчик символов обнуляем
-         symbolСount = 0;
-         // Количество столбцов в строке обнуляем
-         сolumn = 0;
-         if (stroka != null)
-         {
+            // Разделение строки на подстроки по пробелу и конвертация подстрок в double
+            Console.WriteLine("Массив вещественных чисел {0}", nameArray);
+            // Одномерный массив вещественных чисел
+            arrayDouble = new double[сolumn];
+            // Построитель строк
+            StringBuilder stringModified = new StringBuilder();
+            // Счетчик символов обнуляем
+            symbolСount = 0;
+            // Количество столбцов в строке обнуляем
+            сolumn = 0;
             while (symbolСount < stroka.Length)
             {
                if (symbolSpace != stroka[symbolСount])
@@ -117,6 +120,8 @@ namespace Homework_4._6
       {
          Console.WriteLine("Массив вещественных чисел {0} для проведения поиска", nameArray);
          double[] outputArray = new double[n];
+
+
          int i = 0;
          while (i < n)
          {
