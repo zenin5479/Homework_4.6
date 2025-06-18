727311e01b333060313df08c240575e504c16a99
zenin5479 <zenin5479@gmail.com>
2025-06-18 13:33:16 +0300

    Update 9.4 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -104,28 +104,15 @@ namespace Homework_4._6
             string[] arrayTwo = ClassFor1DArray.VivodStringArray(replacingTwo);
             ClassFor1DArray.FileAppendString(arrayTwo, pathFour);
          }
-         
+
          double[] sourceThree = ClassFor1DArray.VvodArray(pathThree, nameThree);
-         if (sourceOne.Length == 0 || sourceTwo.Length == 0 || sourceThree.Length == 0)
+         if (sourceThree.Length == 0)
          {
             Console.WriteLine("Исходный строковый массив пуст");
          }
          else
          {
-            double[] searchOne = ClassFor1DArray.InputArray(sourceOne, elementsOne, nameOne);
-            double[] searchTwo = ClassFor1DArray.InputArray(sourceTwo, elementsTwo, nameTwo);
             double[] searchThree = ClassFor1DArray.InputArray(sourceThree, elementsThree, nameThree);
-
-            double maxOne = ClassFor1DArray.FindMax1DArray(searchOne, nameOne);
-            double[] replacingOne = ClassFor1DArray.ReplacingMax(searchOne, maxOne);
-            string[] arrayOne = ClassFor1DArray.VivodStringArray(replacingOne);
-            ClassFor1DArray.FileAppendString(arrayOne, pathFour);
-
-            double maxTwo = ClassFor1DArray.FindMax1DArray(searchTwo, nameTwo);
-            double[] replacingTwo = ClassFor1DArray.ReplacingMax(searchTwo, maxTwo);
-            string[] arrayTwo = ClassFor1DArray.VivodStringArray(replacingTwo);
-            ClassFor1DArray.FileAppendString(arrayTwo, pathFour);
-
             double maxThree = ClassFor1DArray.FindMax1DArray(searchThree, nameThree);
             double[] replacingThree = ClassFor1DArray.ReplacingMax(searchThree, maxThree);
             string[] arrayThree = ClassFor1DArray.VivodStringArray(replacingThree);
